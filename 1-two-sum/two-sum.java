306d92bec11e3f9b126e02f937e4b269f13b0de6
Vaibhav Agarwal <vaibhav.agarwal.aiml.2023@mitmeerut.ac.in>
2026-08-11 11:27:30 +0530

    Time: 3 ms (58.91%) | Memory: 47.3 MB (11.21%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // First loop iterates through every number except the last one
-        for (int i = 0; i < nums.length - 1; i++) {
-            // Second loop looks at every number AFTER the index 'i'
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j }; // Returns the indices immediately
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
             }
+
+            map.put(nums[i], i);
         }
-        // Return an empty array if no solution is found (though the problem says one always exists)
-        return new int[] {}; 
+
+        return new int[]{};
     }
 }
